93b3848ce45ed555ddc1e96d2340121aa1602a23
sidratul <muhammad.muntaha@lifepal.co.id>
2023-06-09 21:32:24 +0700

    add transition to nav

--- islands/MobileNavigation.tsx
+++ islands/MobileNavigation.tsx
@@ -11,7 +11,7 @@ export default function MobileNavigation() {
         onClick={()=>setActive(true)}
         active={active}
       />
-      <div class={`absolute top-0 max-w-full ${active? "left-0 w-full" : "w-[356px] left-[-356px]"}`} onClick={()=>setActive(false)}>
+      <div class={`transition-all duration-300 absolute top-0 max-w-full ${active? "left-0 w-full" : "w-[356px] left-[-356px]"}`} onClick={()=>setActive(false)}>
         <div class="w-[356px] max-w-[85%]">
           <Navigation key="navigation"/>
         </div>
